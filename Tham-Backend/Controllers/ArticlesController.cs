5e83f86c0ff9d2c26c9a2c5d8e7988f74d02d16f
Pakorn Thanaprachanon <Pakorn.Thanaprachanon@outlook.com>
2022-04-12 18:13:23 +0700

    Test on memory database article

--- Tham-Backend/Controllers/ArticlesController.cs
+++ Tham-Backend/Controllers/ArticlesController.cs
@@ -7,23 +7,59 @@ namespace Tham_Backend.Controllers;
 [ApiController]
 public class ArticlesController : ControllerBase
 {
-    [HttpGet]
-    public async Task<ActionResult<List<Article>>> Get()
+    private static readonly List<Article> Articles = new()
     {
-        var articles = new List<Article>
+        new Article
         {
-            new()
-            {
-                Id = 123456, Title = "สาหวาดดีค้าบ ท่านสมาชิก", Content = "จัดมาดิ๊!", Visible = true,
-                BloggerId = 987654
-            },
-            new()
-            {
-                Id = 123457, Title = "แล้วผมจะไปแคร์ เหี้ยอะไร", Content = "จัดมาดิ๊!", Visible = true,
-                BloggerId = 987654
-            }
-        };
+            Id = 123456, Title = "สาหวาดดีค้าบ ท่านสมาชิก", Content = "จัดมาดิ๊!", Visible = true,
+            BloggerId = 987654
+        },
+        new Article
+        {
+            Id = 123457, Title = "แล้วผมจะไปแคร์ เหี้ยอะไร", Content = "ออกไป๊!", Visible = true,
+            BloggerId = 987654
+        }
+    };
 
-        return Ok(articles);
+    [HttpGet]
+    public async Task<ActionResult<List<Article>>> GetAllArticle()
+    {
+        return Ok(Articles);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Article>> GetArticle(int id)
+    {
+        var article = Articles.Find(article => article.Id == id);
+        if (article == null) return BadRequest("Article not found!");
+        return Ok(article);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<List<Article>>> AddArticle([FromBody] Article article)
+    {
+        Articles.Add(article);
+        return Ok(Articles);
+    }
+
+    [HttpPut]
+    public async Task<ActionResult<List<Article>>> UpdateArticle([FromBody] Article request)
+    {
+        var article = Articles.Find(article => article.Id == request.Id);
+        if (article == null) return BadRequest("Article not found!");
+        article.Content = request.Content;
+        article.Title = request.Title;
+        article.Visible = request.Visible;
+        article.BloggerId = request.BloggerId;
+        return Ok(article);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult<Article>> DeleteArticle(int id)
+    {
+        var article = Articles.Find(article => article.Id == id);
+        if (article == null) return BadRequest("Article not found!");
+        Articles.Remove(article);
+        return Ok(article);
     }
 }
