ef9bf2e14ceb453f8a5403150b0c6a63f62a7de5
Pakorn Thanaprachanon <Pakorn.Thanaprachanon@outlook.com>
2022-04-12 17:53:14 +0700

    Demo article with swagger schema

--- Tham-Backend/Controllers/ArticlesController.cs
+++ Tham-Backend/Controllers/ArticlesController.cs
@@ -1,15 +1,29 @@
 using Microsoft.AspNetCore.Mvc;
+using Tham_Backend.Models;
 
 namespace Tham_Backend.Controllers;
 
-[Route("api/[controller]")]
+[Route("[controller]")]
 [ApiController]
 public class ArticlesController : ControllerBase
 {
-    // GET: api/articles
-    [HttpGet("")]
-    public IActionResult Test()
+    [HttpGet]
+    public async Task<ActionResult<List<Article>>> Get()
     {
-        return Ok("None");
+        var articles = new List<Article>
+        {
+            new()
+            {
+                Id = 123456, Title = "สาหวาดดีค้าบ ท่านสมาชิก", Content = "จัดมาดิ๊!", Visible = true,
+                BloggerId = 987654
+            },
+            new()
+            {
+                Id = 123457, Title = "แล้วผมจะไปแคร์ เหี้ยอะไร", Content = "จัดมาดิ๊!", Visible = true,
+                BloggerId = 987654
+            }
+        };
+
+        return Ok(articles);
     }
 }
--- Tham-Backend/Models/Article.cs
+++ Tham-Backend/Models/Article.cs
@@ -3,9 +3,8 @@
 public class Article
 {
     public int Id { get; set; }
-    public string Title { get; set; }
-    public string Content { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Content { get; set; } = string.Empty;
     public bool Visible { get; set; }
-
-    public int BloggerId { get; set; }//FK
+    public int BloggerId { get; set; } //FK
 }
