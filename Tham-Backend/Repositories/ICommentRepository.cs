03f9fe71a4881d06bc4e7c109fdb601e1a7e52d9
Tanapol Wong-asa <korn_tanapol@outlook.co.th>
2022-04-23 21:49:23 +0700

    Fix comments response

--- Tham-Backend/Repositories/ICommentRepository.cs
+++ Tham-Backend/Repositories/ICommentRepository.cs
@@ -4,8 +4,8 @@ namespace Tham_Backend.Repositories;
 
 public interface ICommentRepository
 {
-    Task<List<CommentModel>> GetCommentsAsync();
-    Task<CommentModel?> GetCommentByIdAsync(int commentId);
+    Task<List<Comments>> GetCommentsAsync();
+    Task<Comments?> GetCommentByIdAsync(int commentId);
     Task<int> AddCommentAsync(CommentModel commentModel);
     Task UpdateCommentAsync(int commentId, CommentModel commentModel);
     Task DeleteCommentAsync(int commentId);
